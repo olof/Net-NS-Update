ec24fbca4465b23c34041cf028cb18f35a3c48d9
Olof Johansson <olof@ethup.se>
2011-04-16 12:28:47 +0200

    Introduced Net::Bind9::Update as baseclass

---
+++ lib/Net/Bind9/Update/nsupdate.pm
@@ -0,0 +1,125 @@
+#!/usr/bin/perl
+# Net::Bind9::Update::nsupdate, Perl module wrapper for nsupdate
+# 
+# Copyright (c) 2011, Olof Johansson <olof@cpan.org> 
+# All rights reserved.
+# 
+# This program is free software; you can redistribute it 
+# and/or modify it under the same terms as Perl itself.  
+
+=head1 NAME
+
+Net::Bind9::Update::nsupdate - Net::Bind9::Update module for wrapping nsupdate
+
+=head1 SYNOPSIS
+
+ my $update = Net::Bind9::Update->new(
+         origin => 'example.com.',
+	 ttl => 3600,
+	 keyfile => '/etc/bind/session.key',
+	 backend => 'nsupdate',
+ );
+
+=head1 DESCRIPTION
+
+Update your dynamic zones directly from Perl. This module is a simple
+wrapper around the nsupdate program, distributed as part of Bind9. You
+start by adding some instructions to the module, and then run execute.
+
+=cut
+
+package Net::Bind9::Update::nsupdate;
+use feature qw/say/;
+use warnings;
+use strict;
+use parent qw/Net::Bind9::Update/;
+use Carp;
+use File::Temp qw/tempfile/;
+our $VERSION = 0.1;
+
+=head1 METHODS
+
+=head2 execute
+
+Execute the instructions added using nsupdate. 
+
+=cut
+
+sub execute {
+	my $self = shift;
+	
+	# weed out deleted instructions (see the undo method)
+	my @instructions = grep {$_} @{$self->{instructions}};
+
+	my($fh, $tmpfile) = tempfile(
+		'nsupdate-XXXXXX', DIR=>$self->{datadir}
+	) or do {
+		$self->{error} = "Could not open tempfile: $!";
+		return undef;
+	};
+
+	say $fh sprintf("server %s %s", 
+		$self->{server}, $self->{port} // '' 
+	) if $self->{server};
+	
+	say $fh sprintf("local %s %s", 
+		$self->{server}, $self->{port} // '' 
+	) if $self->{server};
+	
+	push @instructions, 'send';
+	foreach(@instructions) {
+		say $fh $_;
+	}
+
+	# Ceci n'est pas une pipe
+	open(my $pipe, $self->_get_cmd($tmpfile) . '|') or do {
+		$self->{error} = "Couldn't open pipe: $!";
+		return undef;
+	}; 
+
+	while(<$pipe>) {
+		# Treat all output from nsupdate as fatal errors.
+		# bad? probably
+		chomp;
+		$self->{error} = $_;
+		return undef;
+	}
+	close $pipe;
+	unlink $tmpfile;
+	
+	# clear the list of instructions
+	$self->{instructions} = [];
+
+	return 1;
+}
+
+=head1 AVAILABILITY
+
+Latest stable version is availabe at CPAN:
+
+ L<http://search.cpan.org/perldoc?Net::Bind9::Update::nsupdate>
+
+Git repository (VCS) is available through Github:
+
+ L<http://github.com/olof/Net-Bind9-Update>
+
+=head1 SEE ALSO
+
+=over
+
+=item * Net::Bind9::Update
+
+=item * nsupdate(1)
+
+=item * Bind Administrator's Reference Manual, chapter 4, section 
+        on dynamic updates
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright (c) 2011, Olof Johansson <olof@cpan.org>. All rights reserved.
+
+This program is free software; you can redistribute it and/or modify it 
+under the same terms as Perl itself.  
+
--- t/00-load.t
+++ t/00-load.t
@@ -1,11 +1,13 @@
 #!perl -T
 
-use Test::More tests => 1;
+use Test::More tests => 2;
 
 BEGIN {
+	use_ok( 'Net::Bind9::Update' ) || print "Bail out!
+";
 	use_ok( 'Net::Bind9::Update::nsupdate' ) || print "Bail out!
 ";
 }
 
-diag( "Testing Net::Bind9::Update::nsupdate " . 
+diag( "Testing Net::Bind9::Update " . 
       "$Net::Bind9::Update::VERSION, Perl $], $^X" );
